2cae1d42d4b8f46a1021164969718074bdfec4bd
testror <testpovio@gmail.com>
2015-11-28 20:37:37 +0000

    added user_id to posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+    has_many :posts
+    
     before_save {self.email=email.downcase}
     validates :name, presence: true, length: {minimum:3, maximum:40} 
     VALID_EMAIL_REGEX = /\A[\w+\-,]+@[a-z\d\-,]+\.[a-z]+\z/i
