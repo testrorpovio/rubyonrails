90d0fc35dea675d32fb3e350605d69ef457d2db3
testror <testpovio@gmail.com>
2015-11-28 16:18:17 +0000

    added desc order of the posts showing in index and made 10 posts to show per page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController 
     def index
-        @posts = Post.all.paginate(page: params[:page], per_page: 6).order("created_at desc")
+        @posts = Post.all.paginate(page: params[:page], per_page: 10).order("created_at desc")
     end
 end
