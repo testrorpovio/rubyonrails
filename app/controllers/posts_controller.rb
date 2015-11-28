e02103831b25fa51b8e9f51f90b8fa3bbb10f9f6
testror <testpovio@gmail.com>
2015-11-28 16:17:44 +0000

    added desc order of the posts showing in index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController 
     def index
-        @posts = Post.all.paginate(page: params[:page], per_page: 6)
+        @posts = Post.all.paginate(page: params[:page], per_page: 6).order("created_at desc")
     end
 end
