21947671315558d35cbc39b4f02359bcdf9e9fa4
testror <testpovio@gmail.com>
2015-11-28 16:31:55 +0000

    created page for adding a new post that is showing on the top of the other posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,24 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all.paginate(page: params[:page], per_page: 10).order("created_at desc")
     end
+    
+    def new
+        @post = Post.new
+    end
+    
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            flash[:success] = "The post is successfully created"
+            redirect_to posts_path
+        else
+            render :new
+        end
+    end
+    
+    private
+    
+    def post_params
+        params.require(:post).permit(:title, :description, :username, :created_at, :updated_at)
+    end
 end
